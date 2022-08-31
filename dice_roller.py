49e2b2b22bbc3f4fafca5a097eea15b5e27f28fc
Paramjot Singh <theparamjotsingh@gmail.com>
2022-08-31 20:04:46 +0530

    Added new features and commented out code

--- dice_roller.py
+++ dice_roller.py
@@ -1,15 +1,28 @@
 import random
 
-
-
+# Main method
 def main():
-
+     # Initializing Variables
      dice_sum = 0
-     dice_rolls = 2
+     dice_rolls = int(input('How many dice would you like to roll?'))
+     dice_size = int(input('How many sides is the dice of?'))
+     
+     # Loop for dice roll(s)
      for i in range(0,dice_rolls):
-          roll = random.randint(1,6)
+          roll = random.randint(1,dice_size) # Creating a dice roll
+
+          # Some fun comments
+          if roll == 1: 
+               print(f'You rolled a {roll}! Critical Fail')
+          elif roll == dice_size: #
+               print(f'You rolled a {roll}! Critical Success!')
+          else:
+               print(f'You rolled a {roll}')
+          
+          # Finding the dice sum rolled
           dice_sum += roll
      print(f'You rolled a total of: {dice_sum}')
 
+# Running the main function
 if __name__== "__main__":
      main()
